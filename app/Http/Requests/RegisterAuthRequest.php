8f58a4ad8517412ba3f1f04d74e5f83378e5abf8
tony.jicheng <am362411@gmail.com>
2019-01-20 14:44:08 +0900

    fix 403 forbidden

--- app/Http/Requests/RegisterAuthRequest.php
+++ app/Http/Requests/RegisterAuthRequest.php
@@ -13,7 +13,7 @@ class RegisterAuthRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('login', 'ApiController@login');
 Route::post('register', 'ApiController@register');
 
