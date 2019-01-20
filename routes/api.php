68563263caaf8eb93669657249e6796e47c90510
tony.jicheng <am362411@gmail.com>
2019-01-20 14:08:56 +0900

    add routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,12 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', 'ApiController@login');
+Route::post('register', 'ApiController@register');
+
+Route::group(['middleware' => 'auth.jwt'], function () {
+    Route::get('logout', 'ApiController@logout');
+
+    Route::get('user', 'ApiController@getAuthUser');
+});
